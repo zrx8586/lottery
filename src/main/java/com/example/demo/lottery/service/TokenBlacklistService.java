89e23aac06c1305cf024e8e99628c2ec5293f166
long_w <long_w@trip.com>
2025-04-28 19:59:36 +0800

    删除无用的包引入

--- src/main/java/com/example/demo/lottery/service/TokenBlacklistService.java
+++ src/main/java/com/example/demo/lottery/service/TokenBlacklistService.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
-import java.util.HashSet;
-import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 @Service
